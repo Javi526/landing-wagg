a24b3a8165339a879e07c922078c0a58d8d76c8c
jsegovia <jsegovia@buendoc.com.ar>
2022-10-29 15:51:27 -0300

    adding change text

---
+++ components/terms-conditions/header/index.tsx
@@ -0,0 +1,9 @@
+export default function TermsConditionsHeader() {
+     return (
+         <div className={"TermsConditionsHeader-container"}>
+           <div className={"TermsConditionsHeader-container-content"}>
+              <p className={"TermsConditionsHeader-content-text"}>Téminos y Condiciones (Nombre del link del footer)</p>
+           </div>
+         </div>
+     )
+}
--- pages/terms-conditions/index.tsx
+++ pages/terms-conditions/index.tsx
@@ -1,25 +1,14 @@
-//import Header from "../../components/header/initial";
-
-import Initials from "../../components/landing/principal/initials";
 import Header from "../../components/header/initial";
-import HowDoesItWork from "../../components/landing/principal/how-does-it-work";
-import WhyUse from "../../components/landing/principal/why-use";
-import Plan from "../../components/landing/principal/plan";
-import OurTeam from "../../components/landing/principal/our-teams";
-import Organizations from "../../components/landing/principal/organizations";
-import Impact from "../../components/landing/principal/Impact";
+import Footer from "../../components/footer";
+import TermsConditionsHeader from "../../components/terms-conditions/header";
+
 
 export default function TermsConditions() {
     return (
         <div className={""}>
             <Header />
-            <Initials />
-            <HowDoesItWork />
-            <WhyUse />
-            <Plan />
-            <OurTeam />
-            <Organizations />
-            <Impact />
+            <TermsConditionsHeader />
+            <Footer />
         </div>
     )
 }
