59d831b1085aefbb239bde18a9e6bfce7387947f
WAGG-JSegovia <116173080+WAGG-JSegovia@users.noreply.github.com>
2022-11-09 14:21:40 -0300

    adding change space

--- components/common/text-register/index.tsx
+++ components/common/text-register/index.tsx
@@ -1,9 +1,11 @@
 import { useRouter } from "next/router";
+import {ROUTER} from "../../../constants/router";
 
 export default function TextRegister({ questionText, questionLink, url } : { questionText: string, questionLink: string, url: string }) {
     const router = useRouter();
+    const pathLogin = router.pathname === ROUTER.login ? "64px" : "220px";
     return (
-        <div className={"TextRegister-container"}>
+        <div className={"TextRegister-container"} style={{ marginBottom: pathLogin }}>
             <p className={"ResponsiveMenu-text"}>
                 {questionText}
                 <strong className={"ResponsiveMenu-text-login"} onClick={() => {
