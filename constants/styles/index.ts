0d1258bce29b4cf399883839660771bfb8a486e9
WAGG-JSegovia <116173080+WAGG-JSegovia@users.noreply.github.com>
2022-11-30 11:25:51 -0300

    adding my account viewport v4 modal

--- constants/styles/index.ts
+++ constants/styles/index.ts
@@ -3,10 +3,15 @@ export const styles_Modal = `
      height: 100vh;
      overflow: hidden;
    }
-   @supports(height: 1dvh) {
+   @supports(height: 1svh) {
+       body, main {
+        height: 100svh;
+        overflow: hidden;
+     }
+   @supports(height: 1lvh) {
        body, main {
         height: 100lvh;
         overflow: hidden;
-     }
+     }  
    }
 `;
