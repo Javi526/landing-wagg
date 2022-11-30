f6f1b48efe7c2ad7c6b5fc886ac1dd1b3ec63b86
WAGG-JSegovia <116173080+WAGG-JSegovia@users.noreply.github.com>
2022-11-30 13:49:35 -0300

    adding my account viewport v9 modal

--- constants/styles/index.ts
+++ constants/styles/index.ts
@@ -1,12 +1,9 @@
+const vh = typeof window === "object" && window?.innerHeight;
 export const styles_Modal = `
    body, main {
      height: 100vh;
+     height: calc(var(${vh}px, 1vh) * 100);
      overflow: hidden;
    }
-  
- @supports (-webkit-touch-callout: none) {
-  html,body, main {
-    height: -webkit-fill-available;
-  }
-}
+
 `;
